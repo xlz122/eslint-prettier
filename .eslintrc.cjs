1f6f97dcefa384d291186306c2afa0b4c69b5c24
xlz122 <122874722@qq.com>
2023-12-28 09:53:05 +0800

    chore: 更新vite 5.0.8

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   rules: {
     // 是否使用prettier(error/off)
     'prettier/prettier': 'error',
+
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     
